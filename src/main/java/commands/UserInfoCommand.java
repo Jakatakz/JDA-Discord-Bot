ed2ac041914c92c150484f8a73c2cf1125efe8c2
Jakatakz <Jakatakajak@gmail.com>
2019-07-01 15:26:41 -0400

    waiters for userinfocommand

--- src/main/java/commands/UserInfoCommand.java
+++ src/main/java/commands/UserInfoCommand.java
@@ -3,6 +3,7 @@ package commands;
 import java.awt.Color;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
@@ -11,6 +12,7 @@ import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public class UserInfoCommand extends Command
 {
@@ -24,22 +26,21 @@ public class UserInfoCommand extends Command
 		super.category = new Category("Members");
 		super.cooldown = 10;
 		super.arguments = "[name]";
-		this.waiter = waiter;
+		this.waiter = waiter; 
 	}
 	
 	@Override
 	protected void execute(CommandEvent event)
 	{
-		if (event.getArgs().isEmpty())
-		{
-			event.reply("Provide a name. like this: !uicommand [name]");
-		} else 
-		{
-			Member name;
 			DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			
+			event.reply("give name of user for their info: like @Baboon");
+			waiter.waitForEvent(GuildMessageReceivedEvent.class, e -> e.getAuthor().equals(event.getAuthor()) 
+					&& e.getChannel().equals(event.getChannel()), e -> {
+			
 			try 
 			{
-				name = event.getMessage().getMentionedMembers().get(0);
+				Member name = e.getMessage().getMentionedMembers().get(0);
 				EmbedBuilder eb = new EmbedBuilder()
 						.setColor(Color.magenta)
 						.setThumbnail("https://images-i.jpimedia.uk/imagefetch/c_fill,f_auto,h_1133,q_auto:eco,w_1700/https://inews.co.uk/wp-content/uploads/2016/09/pepehead.jpg")
@@ -61,9 +62,7 @@ public class UserInfoCommand extends Command
 				ex.printStackTrace();
 				event.reply("You need to provide name as a mention.");
 			}
-			
-		}
-		
+					}, 7, TimeUnit.SECONDS, () -> event.reply("no name to search, time expired"));
 	}
 	
 	private String displayGameInfo(Member name)
